28cf73ee216a5d53dd12e3333fef53cab3ab525b
Rümeysa <ryavuz@avans.nl>
2022-03-27 21:41:07 +0200

    [ADDED] New Counter file

---
+++ Lesson-3/B1-Counter/Counter/Counter/lcd,h.h
@@ -0,0 +1,16 @@
+/*
+ * lcd_h.h
+ *
+ * Created: 27-3-2022 21:38:44
+ *  Author: Avans
+ */ 
+
+
+#ifndef LCD,H_H_
+#define LCD,H_H_
+
+
+
+
+
+#endif /* LCD,H_H_ */
--- Lesson-3/B1-Counter/Counter/Counter/main.c
+++ Lesson-3/B1-Counter/Counter/Counter/main.c
@@ -4,14 +4,47 @@
  * Author : Rümeysa Yavuz
  */ 
 
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include "lcd.h"
 
+#define CompareValue 1;
 
-int main(void)
-{
+void timer2Init(void);
+
+void wait( int ms ) {
+	for (int i = 0; i < ms; i++) {
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+int amountOfTimesPressed;
+ISR (TIMER2_COMP_vect) {
+	amountOfTimesPressed++;
+}
+
+void timer2Init(void) {
+	OCR2 = CompareValue;
+	TIMSK |= (1 << 7);
+	TCCR2 = 0x1F;
+	sei();
+}
+
+int main(void) {
+	DDRA = 0xFF;
+	DDRD &= ~(1 << 7);
+	lcd_init();
+	timer2Init();
+	
     /* Replace with your application code */
-    while (1) 
-    {
-    }
+    while (1) {
+		char buffer[10];
+		lcd_set_cursor(0x0);
+		lcd_display_text(itoa(amountOfTimesPressed, buffer, 10));
+		wait(75);
+	}
 }
 
